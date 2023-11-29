4c779940ddd31a242c39c523cd867e6e68558dc9
Akuplace <103694909+Akuplace@users.noreply.github.com>
2023-11-29 00:37:31 +0000

    feat: initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	let baubau = 0;
+	let baubauNoise: HTMLAudioElement;
+
+	function playBaubau() {
+		let audioClone: HTMLAudioElement = baubauNoise.cloneNode(false) as HTMLAudioElement;
+		baubau++;
+
+		audioClone.play();
+
+		audioClone.addEventListener('ended', function () {
+			this.remove();
+		});
+	}
+
+	onMount(() => {
+		baubauNoise = new Audio('/noises/baubau.mp3');
+		baubauNoise.preload = 'auto';
+
+		return baubauNoise.remove;
+	});
+</script>
+
+<h1>{baubau}</h1>
+<button on:click={playBaubau}>Bau!</button>
